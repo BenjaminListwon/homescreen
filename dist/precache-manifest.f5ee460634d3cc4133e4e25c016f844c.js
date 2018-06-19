20f5ade577df19499484923d0c6b46cf5a47f9e0
Benjamin Listwon <blistwon@mac.com>
2018-06-19 16:42:12 -0700

    Adding dist build so don't have to build on the Pi

---
+++ dist/precache-manifest.f5ee460634d3cc4133e4e25c016f844c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "71f1d75a36bb080f8c39929815533e53",
+    "url": "/css/weather-icons-wind.css"
+  },
+  {
+    "revision": "23c04119335532d90f3c18d25bbd5490",
+    "url": "/sample_data/darksky.json"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/font/weathericons-regular-webfont.ttf"
+  },
+  {
+    "revision": "b52fa5a2dd15bfda082a",
+    "url": "/js/chunk-vendors.b52fa5a2.js"
+  },
+  {
+    "revision": "a4ce3e3c4a25e259d3f1",
+    "url": "/js/app.a4ce3e3c.js"
+  },
+  {
+    "revision": "e525c03fefa9e3ed4c5d44ff3277e5b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/font/weathericons-regular-webfont.woff2"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/font/weathericons-regular-webfont.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/font/weathericons-regular-webfont.svg"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/font/weathericons-regular-webfont.eot"
+  },
+  {
+    "revision": "556b77df941d48f44f1fb0d1fec61bc3",
+    "url": "/css/weather-icons.min.css"
+  },
+  {
+    "revision": "81ca3274627ca615962c237698f97f9e",
+    "url": "/css/weather-icons.css"
+  },
+  {
+    "revision": "9e257855b7777b19645c63225f9acb72",
+    "url": "/css/weather-icons-wind.min.css"
+  },
+  {
+    "revision": "a4ce3e3c4a25e259d3f1",
+    "url": "/css/app.5bfe0abe.css"
+  }
+];
